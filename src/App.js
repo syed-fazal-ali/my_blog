a40f09c93854cf1f47c9b5b02e057cd95afb23f0
syed-fazal-ali <syedalikazmi922@gmail.com>
2026-08-08 22:18:33 +0500

    initial commtit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is my_blog app
+         These are changes
         </p>
         <a
           className="App-link"
