ca4615b83df447d0e373d6e44b974b1ddfc5646c
syed-fazal-ali <syedalikazmi922@gmail.com>
2026-08-09 11:05:08 +0500

    login component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import login from './login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <login />
         <p>
          This is my_blog app
          These are changes
---
+++ src/login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const login = () =>{
+    return(
+        <h1>This is login page</h1>
+    )
+}
+
+export default login;
