0313e7058bb242d28aa46cbbe4fc49bc0cd8d496
syed-fazal-ali <syedalikazmi922@gmail.com>
2026-08-09 12:49:21 +0500

    h2 added

--- src/login.jsx
+++ src/login.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const login = () =>{
     return(
-        <h1>This is login page</h1>
+        <>
+         <h1>This is login page</h1>
+               <h2>Webpage</h2>
+        </>
+       
     )
 }
 
